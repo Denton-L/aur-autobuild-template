db2bf4a6f21951390a02a4fe8ceb6e16509a888e
Denton Liu <liu.denton@gmail.com>
2017-11-11 03:35:58 -0500

    Add additional makepkg parameters

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+source settings.conf
+
 REPO_DIR="$PWD"/repo
 AUTO_GPG='gpg --batch --no-tty --yes'
 
@@ -10,23 +12,25 @@ then
 	REPO_NAME=local
 fi
 
-source settings.conf
+export GPGKEY="$sign_key"
+export MAKEPKG_CONF="$PWD"/makepkg.conf
+export PACKAGER="$(git show -s --format='%an <%ae>')"
 
 sudo pacman -Syu --noconfirm
 
 rm -rf "$REPO_DIR"
 git submodule foreach bash -c 'git reset --hard && git clean -xdff'
 
-if [ ! -z "$sign_key" ]
+if [ ! -z "$GPGKEY" ]
 then
 	$AUTO_GPG --import package-key.pgp
-	$AUTO_GPG --export --output public-package-key.pgp "$sign_key"
+	$AUTO_GPG --export --output public-package-key.pgp "$GPGKEY"
 
 	sudo pacman-key --init
 	sudo pacman-key -a public-package-key.pgp
-	sudo pacman-key --lsign-key "$sign_key"
+	sudo pacman-key --lsign-key "$GPGKEY"
 
-	SIGNING_ARGS="--sign --key $sign_key"
+	SIGNING_ARGS="--sign --key $GPGKEY"
 fi
 
 $AUTO_GPG --recv-keys "${trustedpgpkeys[@]}"
@@ -40,8 +44,7 @@ for p in "${packages[@]}"
 do
 	pushd "$p"
 	makepkg -si $SIGNING_ARGS --noconfirm
-	mv *.pkg.tar.xz *.pkg.tar.xz.sig "$REPO_DIR"
 	popd
 done
 
-repo-add "$REPO_DIR"/"$REPO_NAME".db.tar.xz "$REPO_DIR"/*.pkg.tar.xz
+repo-add -s "$REPO_DIR"/"$REPO_NAME".db.tar.xz "$REPO_DIR"/*.pkg.tar.xz
